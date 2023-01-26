7cf07ae9309b931522edcf68778ea4902ea18cfe
y <y@Youssoufs-MacBook-Pro.local>
2023-01-26 18:08:40 +0100

    first commit

---
+++ drumkit.js
@@ -0,0 +1,125 @@
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 81) {
+        document.querySelector('#dclap').style.border = '.4rem solid yellow';
+        document.querySelector('#clap').play();
+        document.querySelector('#clap').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 81) {
+        document.querySelector('#dclap').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 83) {
+        document.querySelector('#dhihat').style.border = '.4rem solid yellow';
+        document.querySelector('#hihat').play();
+        document.querySelector('#hihat').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 83) {
+        document.querySelector('#dhihat').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 68) {
+        document.querySelector('#dkick').style.border = '.4rem solid yellow';
+        document.querySelector('#kick').play();
+        document.querySelector('#kick').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 68) {
+        document.querySelector('#dkick').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 70) {
+        document.querySelector('#dopenhat').style.border = '.4rem solid yellow';
+        document.querySelector('#openhat').play();
+        document.querySelector('#openhat').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 70) {
+        document.querySelector('#dopenhat').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 71) {
+        document.querySelector('#dboom').style.border = '.4rem solid yellow';
+        document.querySelector('#boom').play();
+        document.querySelector('#boom').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 71) {
+        document.querySelector('#dboom').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 72) {
+        document.querySelector('#dride').style.border = '.4rem solid yellow';
+        document.querySelector('#ride').play();
+        document.querySelector('#ride').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 72) {
+        document.querySelector('#dride').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 74) {
+        document.querySelector('#dsnare').style.border = '.4rem solid yellow';
+        document.querySelector('#snare').play();
+        document.querySelector('#snare').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 74) {
+        document.querySelector('#dsnare').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 75) {
+        document.querySelector('#dtom').style.border = '.4rem solid yellow';
+        document.querySelector('#tom').play();
+        document.querySelector('#tom').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 75) {
+        document.querySelector('#dtom').style.border = '.4rem solid black';
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 76) {
+        document.querySelector('#dtink').style.border = '.4rem solid yellow';
+        document.querySelector('#tink').play();
+        document.querySelector('#tink').currentTime = 0;
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.keyCode === 76) {
+        document.querySelector('#dtink').style.border = '.4rem solid black';
+    }
+});
